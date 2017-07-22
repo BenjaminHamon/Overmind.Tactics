4f0f3e008ca25ac757ded00a53dafaccb1fef61b
Benjamin Hamon <hamon.benjamin@gmail.com>
2017-07-22 12:08:58 +0200

    Add terain and example map

---
+++ UnityClient/Assets/Maps/TerrainAtlas.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TerrainAtlas" tilewidth="128" tileheight="128" tilecount="45" columns="15">
+ <image source="../Terrain/TerrainAtlas.png" width="1920" height="384"/>
+ <terraintypes>
+  <terrain name="Grass" tile="-1"/>
+  <terrain name="Water" tile="-1"/>
+  <terrain name="Dirt" tile="-1"/>
+ </terraintypes>
+ <tile id="5" terrain="0,0,0,2"/>
+ <tile id="6" terrain="0,0,2,2"/>
+ <tile id="7" terrain="0,0,2,0"/>
+ <tile id="8" terrain="2,2,2,0"/>
+ <tile id="9" terrain="2,2,0,2"/>
+ <tile id="10" terrain="0,0,0,1"/>
+ <tile id="11" terrain="0,0,1,1"/>
+ <tile id="12" terrain="0,0,1,0"/>
+ <tile id="13" terrain="1,1,1,0"/>
+ <tile id="14" terrain="1,1,0,1"/>
+ <tile id="16" terrain="0,0,0,0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="20" terrain="0,2,0,2"/>
+ <tile id="21" terrain="2,2,2,2"/>
+ <tile id="22" terrain="2,0,2,0"/>
+ <tile id="23" terrain="2,0,2,2"/>
+ <tile id="24" terrain="0,2,2,2"/>
+ <tile id="25" terrain="0,1,0,1"/>
+ <tile id="26" terrain="1,1,1,1">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="27" terrain="1,0,1,0"/>
+ <tile id="28" terrain="1,0,1,1"/>
+ <tile id="29" terrain="0,1,1,1"/>
+ <tile id="33" terrain="0,0,0,0"/>
+ <tile id="34" terrain="0,0,0,0"/>
+ <tile id="35" terrain="0,2,0,0"/>
+ <tile id="36" terrain="2,2,0,0"/>
+ <tile id="37" terrain="2,0,0,0"/>
+ <tile id="40" terrain="0,1,0,0"/>
+ <tile id="41" terrain="1,1,0,0"/>
+ <tile id="42" terrain="1,0,0,0"/>
+</tileset>
