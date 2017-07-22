79127a2523b7dc7f627238b3ebbe98a9cb5baa80
Benjamin Hamon <hamon.benjamin@gmail.com>
2017-07-22 18:20:23 +0200

    Add extension methods for Unity transforms and vectors

---
+++ UnityClient/Assets/Unity/TransformExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Overmind.Tactics.UnityClient.Unity
+{
+	public static class TransformExtensions
+	{
+		public static void DestroyAllChildren(this Transform transform)
+		{
+			List<GameObject> children = transform.Cast<Transform>().Select(t => t.gameObject).ToList();
+			foreach (GameObject child in children)
+			{
+				if (Application.isPlaying)
+					UnityEngine.Object.Destroy(child);
+				else
+					UnityEngine.Object.DestroyImmediate(child);
+			}
+		}
+	}
+}
---
+++ UnityClient/Assets/Unity/VectorExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Overmind.Tactics.UnityClient.Unity
+{
+	public static class ModelExtensions
+	{
+		public static UnityEngine.Vector2 ToUnityVector(this Model.Vector2 vector)
+		{
+			return new UnityEngine.Vector2(vector.X, vector.Y);
+		}
+
+		public static Model.Vector2 ToModelVector(this UnityEngine.Vector2 vector)
+		{
+			return new Model.Vector2(vector.x, vector.y);
+		}
+	}
+}
