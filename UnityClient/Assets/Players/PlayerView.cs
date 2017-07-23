8d2148f974c49d39fdf6f3cc24d08263264e7905
Benjamin Hamon <hamon.benjamin@gmail.com>
2017-07-23 11:09:49 +0200

    Add player with character selection

---
+++ UnityClient/Assets/Players/PlayerView.cs
@@ -0,0 +1,100 @@
+﻿using Overmind.Tactics.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Overmind.Tactics.UnityClient
+{
+	public class PlayerView : MonoBehaviour
+	{
+		public Player Model;
+		public string PlayerName { get { return Model.Name; } }
+
+		public Func<IEnumerable<CharacterView>> GetCharacterCollection;
+
+		private CharacterView selection;
+		public CharacterView Selection
+		{
+			get { return selection; }
+			set
+			{
+				if (selection == value)
+					return;
+				selection = value;
+
+				if (selection == null)
+				{
+					selectionIndicator.SetActive(false);
+					selectionIndicator.transform.SetParent(transform, false);
+				}
+				else
+				{
+					selectionIndicator.transform.SetParent(Selection.transform, false);
+					selectionIndicator.GetComponent<SpriteRenderer>().color = Selection.Model.Owner == Model ? Color.green : Color.red;
+					selectionIndicator.SetActive(true);
+				}
+			}
+		}
+
+		[SerializeField]
+		private GameObject selectionIndicator;
+
+		public void UpdateFromModel()
+		{
+			name = String.Format("Player ({0})", Model.Name);
+		}
+
+		public void Start()
+		{
+			Model.TurnStarted += _ => Enable();
+			Model.TurnEnded += _ => Disable();
+		}
+
+		public void Enable()
+		{
+			gameObject.SetActive(true);
+			Selection = GetCharacterCollection().FirstOrDefault();
+		}
+
+		public void Disable()
+		{
+			Selection = null;
+			gameObject.SetActive(false);
+		}
+		
+		public void SelectPrevious()
+		{
+			List<CharacterView> characterCollection = GetCharacterCollection().ToList();
+
+			int newIndex = characterCollection.IndexOf(Selection);
+			if (newIndex == -1)
+				newIndex = characterCollection.Count - 1;
+			else
+			{
+				newIndex -= 1;
+				if (newIndex < 0)
+					newIndex = characterCollection.Count - 1;
+			}
+			
+			Selection = characterCollection.ElementAtOrDefault(newIndex);
+		}
+
+		public void SelectNext()
+		{
+			List<CharacterView> characterCollection = GetCharacterCollection().ToList();
+
+			int newIndex = characterCollection.IndexOf(Selection);
+			if (newIndex == -1)
+				newIndex = 0;
+			else
+			{
+				newIndex += 1;
+				if (newIndex >= characterCollection.Count)
+					newIndex = 0;
+			}
+
+			Selection = characterCollection.ElementAtOrDefault(newIndex);
+		}
+	}
+}
---
+++ UnityClient/Assets/Tests/Players/Test_PlayerView.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Overmind.Tactics.UnityClient.Tests
+{
+	internal class Test_PlayerView : MonoBehaviour
+	{
+		[SerializeField]
+		private PlayerView player;
+		[SerializeField]
+		private List<CharacterView> characterCollection;
+
+		public void Start()
+		{
+			player.GetCharacterCollection = () => characterCollection;
+
+			StartCoroutine(Run());
+		}
+
+		public IEnumerator Run()
+		{
+			Debug.LogFormat(this, "[Test] Starting {0}", nameof(Test_PlayerView));
+
+			Debug.LogFormat(this, "[Test] Running {0}", nameof(Test_Select));
+			yield return Test_Select();
+
+			Debug.LogFormat(this, "[Test] Done");
+		}
+
+		private IEnumerator Test_Select()
+		{
+			player.Selection = characterCollection.First();
+			yield return new WaitForSeconds(3);
+			player.Selection = null;
+			yield return new WaitForSeconds(1);
+
+			player.SelectNext();
+			yield return new WaitForSeconds(1);
+			player.SelectNext();
+			yield return new WaitForSeconds(1);
+			player.SelectNext();
+			yield return new WaitForSeconds(1);
+			player.SelectNext();
+			yield return new WaitForSeconds(1);
+			player.SelectNext();
+			yield return new WaitForSeconds(1);
+			player.Selection = null;
+			yield return new WaitForSeconds(1);
+
+			player.SelectPrevious();
+			yield return new WaitForSeconds(1);
+			player.SelectPrevious();
+			yield return new WaitForSeconds(1);
+			player.SelectPrevious();
+			yield return new WaitForSeconds(1);
+			player.SelectPrevious();
+			yield return new WaitForSeconds(1);
+			player.SelectPrevious();
+			yield return new WaitForSeconds(1);
+			player.Selection = null;
+			yield return new WaitForSeconds(1);
+		}
+	}
+}
