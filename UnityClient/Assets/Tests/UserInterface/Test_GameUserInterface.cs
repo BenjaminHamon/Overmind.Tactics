294800415f3fb2d4432ffdf495c54edd4efb91c4
Benjamin Hamon <hamon.benjamin@gmail.com>
2017-07-23 16:18:43 +0200

    Add game user interface

---
+++ UnityClient/Assets/Tests/UserInterface/Test_GameUserInterface.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Overmind.Tactics.UnityClient.Tests.UserInterface
+{
+	internal class Test_GameUserInterface : MonoBehaviour
+	{
+		[SerializeField]
+		private PlayerView player;
+		[SerializeField]
+		private CharacterView character;
+
+		public void Start()
+		{
+			character.Model.Owner = player.Model;
+			character.UpdateFromModel();
+
+			StartCoroutine(Run());
+		}
+
+		public IEnumerator Run()
+		{
+			Debug.LogFormat(this, "[Test] Starting {0}", nameof(Test_GameUserInterface));
+
+			Debug.LogFormat(this, "[Test] Running {0}", nameof(Test_Update));
+			yield return Test_Update();
+
+			Debug.LogFormat(this, "[Test] Done");
+		}
+
+		private IEnumerator Test_Update()
+		{
+			player.Selection = null;
+			yield return new WaitForSeconds(3);
+
+			player.Selection = character;
+			yield return new WaitForSeconds(3);
+
+			player.Selection = null;
+			yield return new WaitForSeconds(3);
+		}
+	}
+}
---
+++ UnityClient/Assets/UserInterface/GameUserInterface.cs
@@ -0,0 +1,37 @@
+﻿using Overmind.Tactics.Model;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Overmind.Tactics.UnityClient.UserInterface
+{
+	public class GameUserInterface : MonoBehaviour
+	{
+		public PlayerView Player;
+		[NonSerialized]
+		public Game Game;
+
+		[SerializeField]
+		private Text activePlayerText;
+		[SerializeField]
+		private Text turnText;
+		[SerializeField]
+		public CharacterPanel characterPanel;
+
+		public Button EndTurnButton;
+		public Button SelectPreviousCharacterButton;
+		public Button SelectNextCharacterButton;
+		public AbilityPanel AbilityPanel;
+
+		public void Update()
+		{
+			activePlayerText.text = Game?.ActiveState.ActivePlayer?.Name;
+			turnText.text = Game != null ? "Turn " + Game.ActiveState.Turn : null;
+
+			characterPanel.gameObject.SetActive(Player?.Selection != null);
+			characterPanel.Character = Player?.Selection?.Model;
+			AbilityPanel.gameObject.SetActive(Player?.Selection != null);
+			AbilityPanel.Character = Player?.Selection?.Model;
+		}
+	}
+}
